96fbdaba3ee9eae7eb4fbcf860e87c7d855ced67
jaforrockman <jaforrockman695@gmail.com>
2015-02-16 20:30:31 +0600

    re removed registration

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   #: to add registration jsut put this   :registerable ,
-  devise :database_authenticatable, :registerable ,
+  devise :database_authenticatable, 
          :recoverable, :rememberable, :trackable, :validatable
 end
