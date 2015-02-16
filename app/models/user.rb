da8db10577759edd8572c280907c983578a77d52
jaforrockman <jaforrockman695@gmail.com>
2015-02-16 20:26:57 +0600

    added registration

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  #: to add registration jsut put this   registerable ,
-  devise :database_authenticatable, 
+  #: to add registration jsut put this   :registerable ,
+  devise :database_authenticatable, :registerable ,
          :recoverable, :rememberable, :trackable, :validatable
 end
